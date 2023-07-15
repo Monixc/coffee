e16b641199eac8d01938fa5323d5dd58f1b0037c
Monixc <chris13404@gmail.com>
2023-07-16 02:51:05 +0900

    slide animation

--- js/main.js
+++ js/main.js
@@ -44,3 +44,43 @@ fadeEls.forEach(function (fadeEl, index) {
     opacity: 1,
   });
 });
+//슬라이드 기능
+new Swiper(".notice .swiper", {
+  direction: "vertical", //수직 슬라이드
+  autoplay: true, //자동 재생 여부
+  loop: true, //반복 재생 여부
+});
+new Swiper(".promotion .swiper", {
+  autoplay: true, //자동 재생 여부
+  loop: true, //반복 재생 여부
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, //슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+});
+new Swiper(".promotion .swiper", {
+  autoplay: true,
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  pagination: {
+    //페이지 번호 사용
+    el: ".promotion .swiper-pagination", //페이지 번호 요소
+    clickable: true, //사용자의 페이지 번호 제어 여부
+  },
+  navigation: {
+    //슬라이드 이전/다음 버튼 사용
+    prevEl: ".promotion .swiper-button-prev",
+    nextEl: ".promotion .swiper-button-next",
+  },
+});
+
+const promotionEl = document.querySelector("section.promotion");
+const promotionToggleBtn = document.querySelector(".toggle-promotion");
+promotionToggleBtn.addEventListener("click", function () {
+  if (promotionEl.classList.contains("hide")) {
+    promotionEl.classList.remove("hide");
+  } else {
+    promotionEl.classList.add("hide");
+  }
+});
